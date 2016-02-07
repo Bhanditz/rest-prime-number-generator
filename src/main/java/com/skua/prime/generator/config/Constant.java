c11743b032b26253b01ab0718290ff5ffe8fba47
kunwar_singh <kunwar@golaser.co.uk>
2016-02-07 18:11:50 +0000

    Initial commit

---
+++ src/main/java/com/skua/prime/generator/config/Constant.java
@@ -0,0 +1,10 @@
+package com.skua.prime.generator.config;
+
+/**
+ * Created by kunwar.singh on 07/02/2016.
+ */
+public class Constant {
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+
+}
